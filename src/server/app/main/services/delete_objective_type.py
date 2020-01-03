a68fee58db8689f849773b4457c9f00e863e760a
Rohit Goyal <2211rohit@gmail.com>
2020-01-03 16:45:39 +0530

    Add Delete Question Set API

---
+++ src/server/app/main/services/delete_objective_type.py
@@ -0,0 +1,19 @@
+from app.main import db
+from app.main.models.objective_type import ObjectiveType
+from flask import jsonify
+
+
+def delete_objective_type(data):
+    """[summary]   
+    
+    Args:
+        data ([type]): [description]
+    
+    Returns:
+        [type]: [description]
+    """
+    objective_type = ObjectiveType.query.get(int(data["objective_type_id"]))
+    db.session.delete(objective_type)
+    db.session.commit()
+    response_object = jsonify({"response": "successfully deleted"})
+    return response_object, 200
