23786428496da3d0c842873ab822d6999f83d065
Madhu <shuklamadhu72@gmail.com>
2020-01-07 13:54:42 +0530

    Teacher Profile

--- src/client/src/routes/TeacherProfile.jsx
+++ src/client/src/routes/TeacherProfile.jsx
@@ -7,7 +7,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-
+ 
 export default class TeacherProfile extends React.Component {
   constructor(props) {
     super(props);
