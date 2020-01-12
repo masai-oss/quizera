a078b76eeb6a9e5c5ef3d184634ef5d3f84bb663
hrishabh <rishabhofficial88@gmail.com>
2020-01-12 19:56:00 +0530

    Updated: init.py

--- src/server/app/main/routes/__init__.py
+++ src/server/app/main/routes/__init__.py
@@ -1,5 +1,9 @@
 from app.main.routes.auth_controller import UserLogin, LogoutAPI, UserSignUp, get_user_id, TeacherSignUp, StudentSignUp, TeacherLogin, StudentLogin
 from app.main import api
+from app.main.routes.leaderboard import Leaderboard
+from .blueprint_admin import admin
+from .blueprint_student import student
+from .blueprint_teacher import teacher
 
 def add_resources(app):
     """
@@ -19,6 +23,7 @@ def add_resources(app):
     api.add_resource(UserSignUp, '/admin/register')
     api.add_resource(UserLogin, '/admin/login')
     api.add_resource(LogoutAPI, '/logout')
+    api.add_resource(Leaderboard, '/leaderboard')
 
 def register_blueprints(app):
     """
@@ -27,4 +32,6 @@ def register_blueprints(app):
     Args:
         app (object): object of Flask representing the app in context
     """
-    pass
+    app.register_blueprint(admin, url_prefix="/admin")
+    app.register_blueprint(student, url_prefix="/student")
+    app.register_blueprint(teacher, url_prefix="/teacher")
