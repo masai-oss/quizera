188e2526cd3aab864ddc41b6680100b7b2db701c
albseb511 <albertsebastian511@gmail.com>
2020-01-15 00:17:19 +0530

    Fix: Student Login Title

--- src/client/src/routes/Login.jsx
+++ src/client/src/routes/Login.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -43,17 +42,17 @@ export default class Login extends React.Component {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className="mt-5" style={{ 
-       display: 'flex',
-       marginTop:'100px',
-       flexDirection: 'column',
-       alignItems: 'center' }}>
+       display: "flex",
+       marginTop:"100px",
+       flexDirection: "column",
+       alignItems: "center" }}>
         <Avatar className="m-1" style={{ backgroundColor: "#f44336", }}>
         </Avatar>
         <Typography component="h1" variant="h5">
-          Login
+          Student Login
         </Typography>
-        <form className=" " style={{ width: '100%', // Fix IE 11 issue.
-                      marginTop: '20px'}} noValidate>
+        <form className=" " style={{ width: "100%", // Fix IE 11 issue.
+                      marginTop: "20px"}} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -100,7 +99,7 @@ export default class Login extends React.Component {
             </Grid>
             <Grid item className="mt-5">
               <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
+                {"Dont have an account? Sign Up"}
               </Link>
             </Grid>
           </Grid>
