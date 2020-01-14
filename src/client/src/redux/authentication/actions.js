bdb898c3ada3ceb32936b801d5dacc3f38d56e9e
hrishabh <rishabhofficial88@gmail.com>
2020-01-14 22:04:52 +0530

    Added: Admin auth

--- src/client/src/redux/authentication/actions.js
+++ src/client/src/redux/authentication/actions.js
@@ -1,9 +1,35 @@
-import { LOGIN_USER, LOGOUT_USER } from "./actionTypes";
+import {
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from "./actionTypes";
+import axios from "../../utils/axiosInterceptor";
 
-export const loginUser = () => ({
-  type: LOGIN_USER
+export const loginUserRequest = payload => ({
+  type: LOGIN_USER_REQUEST,
+  payload
 });
-
-export const logoutUser = () => ({
-  type: LOGOUT_USER
+export const loginUserSuccess = payload => ({
+  type: LOGIN_USER_SUCCESS,
+  payload
 });
+export const loginUserFail = () => ({
+  type: LOGIN_USER_FAIL
+});
+export const loginUser = payload => {
+  return dispatch => {
+    dispatch(loginUserRequest());
+    console.log(payload);
+    return axios
+      .post("/admin_login", {
+        email: payload.email,
+        password: payload.password
+      })
+      .then(res => {
+        console.log("success", res);
+        localStorage.setItem("quizera_auth_token", res.token);
+        dispatch(loginUserSuccess(res));
+      })
+      .catch(() => dispatch(loginUserFail()));
+  };
+};
