f8c463d34f213f2441bb5917f10a805e68e03f07
albseb511 <albertsebastian511@gmail.com>
2020-01-15 10:54:49 +0530

    Update: Home page content

--- src/client/src/routes/Home.jsx
+++ src/client/src/routes/Home.jsx
@@ -5,22 +5,15 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <header className={styles.header}>
-        <h1>Masai Open Source</h1>
-        <img src="/images/logo.svg" className={styles.reactLogo} alt="logo" />
-        <p>
-          Edit <code>src/routes/Home.js</code> and save to reload.
-        </p>
-        <p>
-          An Initiative By{" "}
-          <a
-            className={styles.link}
-            href="https://www.masaischool.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Masai School
-          </a>
-        </p>
+        <h1>QUIZERA</h1>
+        <br />
+        <div>
+          Quizera is a platorm build for students and teachers to make learning
+          easier
+        </div>
+        <br />
+        <br />
+        <div>Please login to continue</div>
       </header>
     </div>
   );
